15b105c47625622c31b56bd4a8ca8254cd7bd046
Andre Cabral <andre.cabral@grupointegrado.br>
2024-07-07 23:36:24 -0300

    aplicação de pesquisa sobre artista ou banda com chat gpt

---
+++ src/main/java/br/com/alura/ScreenSoudn/model/TipoArtista.java
@@ -0,0 +1,7 @@
+package br.com.alura.ScreenSoudn.model;
+
+public enum TipoArtista {
+    SOLO,
+    DUPLA,
+    BANDA;
+}
---
+++ src/test/java/br/com/alura/ScreenSoudn/ScreenSoudnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.ScreenSoudn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSoudnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
